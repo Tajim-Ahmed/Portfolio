d71eeec6d07c509ba9c5c09b499d7dadd1232b2d
Tajim Ahmed Tasleem Ahmed <tajimahmed360@gmail.com>
2026-08-19 14:18:49 +0530

    removed theme

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,13 @@ import React from 'react'
 import {Button} from "@/components/ui/button"
 import Home from './pages/Home'
 import Cursor from './components/ui/Cursor'
-import Theme from './components/Theme'
+
 
 const App = () => {
   return (
     <div>
       <Cursor />
-      <Theme />
+      
       <Home />
      
     </div>
